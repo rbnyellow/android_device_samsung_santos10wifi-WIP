d983ea2d8e11d7174515fc8538aca516de86abd9
rbnyellow <rbnyellow@gmail.com>
2020-07-24 12:02:18 +0200

    santos10wifi : update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2012 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,14 +19,13 @@ PRODUCT_RELEASE_NAME := santos10wifi
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 800
+TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common Lineage stuff.
+# Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/santos10wifi/device.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := santos10wifi
@@ -36,3 +35,7 @@ PRODUCT_MODEL := GT-P5210
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=santos10wifixx \
+    BUILD_FINGERPRINT="samsung/santos10wifixx/santos10wifi:4.4.2/KOT49H/P5210XXUBOB1:user/release-keys" \
+    PRIVATE_BUILD_DESC="santos10wifixx-user 4.4.2 KOT49H P5210XXUBOB1 release-keys"
